405bfd3bd4e3a099d90bbe22921b36925fcdfa43
niKo-ing <nic.estefania@duocuc.cl>
2025-03-26 18:14:41 -0300

    comentario :3

---
+++ src/main/java/com/holamundo/ejemplo/holamundo/controller/HolaMundoController.java
@@ -0,0 +1,19 @@
+package com.holamundo.ejemplo.holamundo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class HolaMundoController {
+
+
+
+    @GetMapping("/hola")
+    public String holamundo() {
+        return "Hola mundo!";
+    }
+    
+
+}
